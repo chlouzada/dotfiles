45840c092142861b11a84045b8ccfcdd4519ad31
chlouzada <chlouzada@gmail.com>
2023-08-12 16:47:00 -0300

    asdf helper script

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -38,7 +38,6 @@ in
     bat
     fd
     nixfmt
-    asdf-vm
     gh
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
---
+++ home-manager/.config/home-manager/modules/asdf-vm.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    fzf
+    asdf-vm
+
+    (pkgs.writeShellScriptBin "asdfx" ''
+      #!/bin/bash
+
+      selected_plugin=$(asdf plugin-list-all | awk '{print $1}' | fzf)
+
+      if ! asdf plugin-list | grep -q "$selected_plugin"; then
+        asdf plugin-add "$selected_plugin"
+      fi
+
+      selected_version=$(asdf list-all "$selected_plugin" | awk '{print $1}' | tac | fzf)
+  
+      if ! asdf list "$selected_plugin" | grep -q "$selected_version"; then
+        asdf install "$selected_plugin" "$selected_version"
+      fi
+
+      asdf global "$selected_plugin" "$selected_version"
+    '')
+  ];
+}
