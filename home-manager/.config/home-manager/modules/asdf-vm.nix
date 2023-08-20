6e964d0580fc784f33329ae755d02ec8674cf7d1
chlouzada <chlouzada@gmail.com>
2023-08-20 11:50:04 -0300

    update

--- home-manager/.config/home-manager/modules/asdf-vm.nix
+++ home-manager/.config/home-manager/modules/asdf-vm.nix
@@ -1,23 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  programs.fzf = {
-    enable = true;
-  };
-
   home.packages = with pkgs; [
     asdf-vm
 
     (pkgs.writeShellScriptBin "asdfx" ''
       #!/bin/bash
 
-      selected_plugin=$(asdf plugin-list-all | awk '{print $1}' | fzf)
+      selected_plugin=$(asdf plugin-list-all | awk '{print $1}' | ${pkgs.fzf}/bin/fzf)
 
       if ! asdf plugin-list | grep -q "$selected_plugin"; then
         asdf plugin-add "$selected_plugin"
       fi
 
-      selected_version=$(asdf list-all "$selected_plugin" | awk '{print $1}' | tac | fzf)
+      selected_version=$(asdf list-all "$selected_plugin" | awk '{print $1}' | tac | ${pkgs.fzf}/bin/fzf)
   
       if ! asdf list "$selected_plugin" | grep -q "$selected_version"; then
         asdf install "$selected_plugin" "$selected_version"
