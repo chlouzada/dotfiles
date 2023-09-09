5c80b75cd948dc83110f4060c906e2d5b7b714cd
chlouzada <chlouzada@gmail.com>
2023-09-09 09:56:27 -0300

    zip

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -41,6 +41,8 @@ in
     gh
     ripgrep
     youtube-dl
+    zip
+    unzip
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
