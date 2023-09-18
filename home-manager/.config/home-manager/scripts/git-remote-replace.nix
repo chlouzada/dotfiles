4a5941b0d175e8b0001ce5546d904fe4e5445770
chlouzada <chlouzada@gmail.com>
2023-09-17 23:59:47 -0300

    error if no args supplied

--- home-manager/.config/home-manager/scripts/git-remote-replace.nix
+++ home-manager/.config/home-manager/scripts/git-remote-replace.nix
@@ -3,6 +3,11 @@
 {
   home.packages = with pkgs; [
     (pkgs.writeShellScriptBin "git-remote-replace" ''
+      if [ $# -ne 2 ]; then
+        echo "missing args"
+        echo "usage: git-remote-replace {remote_name} {url}"
+        exit 1
+      fi
       remote=$1
       git remote rename $remote replaced-$remote-$(date +%s)
       git remote add $remote $2
