da73260102b013e7d58438901208344e259f7caa
chlouzada <chlouzada@gmail.com>
2024-04-18 13:19:54 -0300

    refact

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -17,25 +17,35 @@ let
   imports = moduleImports ++ scriptImports  ++ (if isWSL then [] else linuxOnlyImports);
 in
 {
-  # read all modules
   imports = imports;
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "ch";
   home.homeDirectory = "/home/ch";
+  
+  programs = {
+    bash = {
+      enable = true;
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+      initExtra = "
+        fish
+      ";
+    };
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    lazygit = {
+      enable = true;
+    };
+
+    fzf = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+  };
+
   home.packages = with pkgs; [
     openssh
     stow
@@ -53,18 +63,6 @@ in
     jq
     tldr
     # curl-impersonate
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    (pkgs.writeShellScriptBin "my-hello" ''
-      echo "Hello, world!"
-    '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -82,16 +80,6 @@ in
     # '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/ch/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     PATH="/home/ch/.turso:$PATH";
     # EDITOR = "code";
@@ -102,6 +90,6 @@ in
     # allowUnsupportedSystem = true;
   };
 
-  # Let Home Manager install and manage itself.
+  home.stateVersion = "23.05";
   programs.home-manager.enable = true;
 }
--- home-manager/.config/home-manager/modules/_exa.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  programs.exa = {
-    enable = true;
-    enableAliases = true;
-  };
-}
