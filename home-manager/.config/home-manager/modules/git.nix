649871c871665a68a9fd6aeb5532ee75fa610172
chlouzada <chlouzada@gmail.com>
2024-04-18 13:13:41 -0300

    git cfg

--- home-manager/.config/home-manager/modules/git.nix
+++ home-manager/.config/home-manager/modules/git.nix
@@ -19,8 +19,14 @@
       push.autoSetupRemote = true;
 
       url."git@github.com:".pushInsteadOf = "https://github.com";
+
       url."git@github.com:".insteadOf = "gh:";
       url."https://github.com/".insteadOf = "gh/:";
+
+      url."git@gitlab.com:".insteadOf = "gl:";
+      url."https://gitlab.com/".insteadOf = "gl/:";
+
+      url."git@gitlab.eurekka.technology:eurekka-equipe-interna/".insteadOf = "eurekka:";
     };
   };
 }
