c5b646de2712b57399b81581ceb2c0b83510a44a
chlouzada <chlouzada@gmail.com>
2026-06-02 17:07:37 -0300

    update git.nix

--- home-manager/.config/home-manager/modules/git.nix
+++ home-manager/.config/home-manager/modules/git.nix
@@ -4,30 +4,45 @@
   programs.git = {
     enable = true;
 
-    userName = "chlouzada";
-    userEmail = "chlouzada@gmail.com"; 
+    settings = {
+      user = {
+        name = "chlouzada";
+        email = "chlouzada@gmail.com";
+      };
 
-    aliases = {
-      co = "checkout";
-      ci = "commit";
-      undo = "!f() { [[ \"\$1\" == \"--hard\" ]] && git reset --hard HEAD~\${2:-1} || git reset --soft HEAD~\${1:-1}; }; f";
-    };
+      alias = {
+        co = "checkout";
+        ci = "commit";
+        undo = "!f() { [[ \"$1\" == \"--hard\" ]] && git reset --hard HEAD~${2:-1} || git reset --soft HEAD~${1:-1}; }; f";
+      };
 
-    extraConfig = {
       init.defaultBranch = "main";
 
-      push.default = "simple"; 
-      push.autoSetupRemote = true;
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+      };
 
-      url."git@github.com:".pushInsteadOf = "https://github.com";
+      url."git@github.com:" = {
+        pushInsteadOf = "https://github.com";
+        insteadOf = "gh:";
+      };
 
-      url."git@github.com:".insteadOf = "gh:";
-      url."https://github.com/".insteadOf = "gh/:";
+      url."https://github.com/" = {
+        insteadOf = "gh/:";
+      };
 
-      url."git@gitlab.com:".insteadOf = "gl:";
-      url."https://gitlab.com/".insteadOf = "gl/:";
+      url."git@gitlab.com:" = {
+        insteadOf = "gl:";
+      };
 
-      url."git@gitlab.eurekka.technology:eurekka-equipe-interna/".insteadOf = "eurekka:";
+      url."https://gitlab.com/" = {
+        insteadOf = "gl/:";
+      };
+
+      url."git@gitlab.eurekka.technology:eurekka-equipe-interna/" = {
+        insteadOf = "eurekka:";
+      };
     };
   };
 }
