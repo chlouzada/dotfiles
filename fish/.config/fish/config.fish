c7f83c7342cd8c58d3cb7f7fedba0348498290bf
chlouzada <chlouzada@gmail.com>
2026-06-23 10:11:10 -0300

    fish cfg

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+#
     set -g fish_greeting
 
     set -Ux ANDROID_HOME $HOME/Android/Sdk
@@ -6,22 +7,37 @@ if status is-interactive
     fish_add_path $ANDROID_HOME/platform-tools
 
     alias s="cd .."
-
     alias fd="fdfind"
+    alias c="editor ."
 
     abbr -a lg lazygit
-    abbr -a ldocker lazydocker
-    abbr -a c "code ."
+    abbr -a ldock lazydocker
+    # abbr -a c "code ."
+    # abbr -a c "codium ."
+    abbr -a d "npm run dev"
+    abbr -a gsub "git submodule update --init --recursive"
 
     ~/.local/bin/mise activate fish | source
 
     bind \e\cf _fzf_search_dir
     function _fzf_search_dir
-        set dir (fd . ~ --type d --max-depth 1 | fzf)
+        set dir (fd . ~ --type d --max-depth 2 | fzf)
 
         if test -n "$dir"
             cd "$dir"
             commandline -f repaint
         end
     end
+
+    function editor
+        if command -q codium
+            codium $argv
+        else if command -q code
+            code $argv
+        else
+            return 1
+        end
+    end
+
+#
 end
